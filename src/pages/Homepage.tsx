491cfdf63f4ed65647cc7dbee32d4736bb874b3d
unknown <klu19@jh.edu>
2025-10-26 18:30:16 -0400

    Resolved build issues

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+
 import Aboutus from "../components/Aboutus";
 import Hero from "../components/Hero";
-import Footer from "../components/Footer";
-import skyline from "../assets/baltimoreskyline.jpg"; 
 
 
 
--- src/pages/Teampage.tsx
+++ src/pages/Teampage.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Hero from "../components/Hero";
+
 import TeamTitle from "../components/TeamTitle";
 import Team from "../components/Team";
 import "./Teampage.css";
